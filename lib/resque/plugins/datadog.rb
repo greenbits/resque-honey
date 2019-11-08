d3b7921baf33b94f56ef0d5f81b918eee7344efa
Gabriel Charleboix <gcharleboix@greenbits.com>
2019-11-08 09:25:55 -0800

    log when job enters queue and when it starts

--- lib/resque/plugins/datadog.rb
+++ lib/resque/plugins/datadog.rb
@@ -4,12 +4,27 @@ module Resque
       mattr_accessor :statsd
       mattr_accessor :service_name
 
+      def after_enqueue_datadog(*args)
+        return unless statsd
+        @enqueued_at = Time.now
+        statsd.event('Job Enqueued', enqueued_at.to_s, tags: job_tags(args))
+      end
+
       def around_perform_datadog(*args)
         return yield unless statsd
 
         exc = nil
         result = nil
         start_time = Time.now
+        statsd.batch do |s|
+          s.event('Job Started', start_time)
+          s.timing(
+            'resque.jobs.enqueued_duration',
+            (start_time - @enqueued_at) * 1000,
+            tags: job_tags(args)
+          )
+        end
+
         begin
           result = yield
         rescue => e
@@ -17,12 +32,7 @@ module Resque
         end
         end_time = Time.now
 
-        tags = [
-          "job_class:#{self.name.to_s}",
-          "queue:#{@queue.to_s}"
-        ]
-        tags << "service:#{service_name}" if service_name.present?
-        tags = tags.concat(dd_tags(*args)).uniq if respond_to?(:dd_tags)
+        tags = job_tags(args)
 
         statsd.batch do |s|
           s.increment('resque.jobs.total', tags: tags)
@@ -32,6 +42,20 @@ module Resque
 
         raise exc if exc
         result
+      end
+
+      private
+
+      def job_tags(args)
+        tags = [
+          "job_class:#{self.name.to_s}",
+          "queue:#{@queue.to_s}"
+        ]
+
+        tags << "service:#{service_name}" if service_name.present?
+        tags = tags.concat(dd_tags(*args)).uniq if respond_to?(:dd_tags)
+
+        tags
       end
     end
   end
